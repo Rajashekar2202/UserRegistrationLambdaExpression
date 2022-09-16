9e1da71e26746388fcc02181f12479e409a1fde3
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-16 19:21:52 +0530

    added Mobile number program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LambdaExpression
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Lambda Expression Problem");
+            
+            UserRegistration user = new UserRegistration();
+            Console.WriteLine("****User Registration****");
+            Console.WriteLine("Enter Mobile Number");
+            string Email = Console.ReadLine();
+            string opt = user.validateMobileNumberusingLambda(MobileNumber);
+            if (opt == "Valid")
+            {
+                Console.WriteLine("Pattern is matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern is not matched");
+            }
+        }
+    }
+}
