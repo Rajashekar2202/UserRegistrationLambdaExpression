5b1118c6f50a49c5a16a8b115f08b56fb51e532b
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-16 19:39:42 +0530

    added Sample Email program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LambdaExpression
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Lambda Expression Problem");
+            
+            UserRegistration user = new UserRegistration();
+            Console.WriteLine("****User Registration****");
+            Console.WriteLine("Enter Password");
+            string Email9 = Console.ReadLine();
+            string opt = user.validateEmail9usingLambda(Email9);
+            if (opt == "Valid")
+            {
+                Console.WriteLine("Pattern is matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern is not matched");
+            }
+        }
+    }
+}
