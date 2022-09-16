9eb37ad7c2074e4d50681130a5610cde5127942d
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-16 19:30:16 +0530

    added Password1 program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LambdaExpression
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Lambda Expression Problem");
+            
+            UserRegistration user = new UserRegistration();
+            Console.WriteLine("****User Registration****");
+            Console.WriteLine("Enter Password");
+            string Password1 = Console.ReadLine();
+            string opt = user.validatePassword1usingLambda(Password1);
+            if (opt == "Valid")
+            {
+                Console.WriteLine("Pattern is matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern is not matched");
+            }
+        }
+    }
+}
