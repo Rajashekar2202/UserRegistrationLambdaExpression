d631b353bd2c6ee08c5a1df43c332b013115c1c9
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-16 19:15:29 +0530

    added LastName program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LambdaExpression
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Lambda Expression Problem");
+            
+            UserRegistration user = new UserRegistration();
+            Console.WriteLine("****User Registration****");
+            Console.WriteLine("Enter Lastname");
+            string LastName = Console.ReadLine();
+            string opt = user.validateLastNameusingLambda(LastName);
+            if (opt == "Valid")
+            {
+                Console.WriteLine("Pattern is matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern is not matched");
+            }
+        }
+    }
+}
