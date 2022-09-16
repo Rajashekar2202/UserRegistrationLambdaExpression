783187804df452e2538ddba9c8df81eba7691d61
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-16 19:11:20 +0530

    added FirstName program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LambdaExpression
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Lambda Expression Problem");
+            
+            UserRegistration user = new UserRegistration();
+            Console.WriteLine("****User Registration****");
+            Console.WriteLine("Enter Firstname");
+            string Firstname = Console.ReadLine();
+            string opt = user.FirstnameCheckusingLambda(Firstname);
+
+            if (opt == "FirstName is Valid")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(opt);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(opt);
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ UserRegistration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LambdaExpression
+{
+    public class UserRegistration
+    {
+        string Firstname = @"[A-Z]{1}[a-z]{2}$";
+        public string FirstnameCheckusingLambda(string firstName) => Regex.IsMatch(firstName, Firstname) ? "FirstName is Valid" : "FirstName is not Valid";
+    }
+}
