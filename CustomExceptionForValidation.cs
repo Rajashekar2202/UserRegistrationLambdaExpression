c5e6dde05627c7e3f550e3c77324c2f0aa332c9a
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-16 20:03:32 +0530

    added Custom expectations program

---
+++ CustomExceptionForValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationLambdaExpression
+{
+    public class CustomException_For_UserValidation : Exception
+    {
+        private ExceptionType type;
+        public enum ExceptionType
+        { 
+            NULL_MESSAGE,
+            EMPTY_MESSAGE,
+            INVALID_MESSAGE
+        }
+
+        public CustomException_For_UserValidation(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using UserRegistrationLambdaExpression;
+
+Console.WriteLine("------ Welcome To USer Validation System ------\n");
+UserValidation.User_Validation();
